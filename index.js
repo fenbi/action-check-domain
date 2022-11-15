4678a00169d0cfd0bc36c371f4ffc8f705709d20
liluo <i@liluo.org>
2022-11-15 15:37:58 +0800

    Added action args

--- index.js
+++ index.js
@@ -9,34 +9,43 @@ try {
      * @type {string}
      */
     const URL = core.getInput('url');
+    const CHECK_ACTION = core.getInput('check_action');
 
-    /**
-     * Check SSL certificate
-     */
-    CheckCertificate(URL)
-        .then(date => {
-            core.setOutput("ssl-expire-date", date.toString());
-            core.setOutput("ssl-expire-days-left", Dates.countDays(date));
-        })
-        .catch(error => {
-            if (error.code === 'CERT_HAS_EXPIRED') {
-                core.setOutput("ssl-expire-date", "INVALID");
-                core.setOutput("ssl-expire-days-left", -1);
-            }
+    core.setOutput("action", CHECK_ACTION);
 
-            throw error;
-        })
-        .catch(core.error);
+    if (CHECK_ACTION == "ssl") {
+        core.setOutput("run-action", "ssl");
+        /**
+         * Check SSL certificate
+         */
+        CheckCertificate(URL)
+            .then(date => {
+                core.setOutput("ssl-expire-date", date.toString());
+                core.setOutput("ssl-expire-days-left", Dates.countDays(date));
+            })
+            .catch(error => {
+                if (error.code === 'CERT_HAS_EXPIRED') {
+                    core.setOutput("ssl-expire-date", "INVALID");
+                    core.setOutput("ssl-expire-days-left", -1);
+                }
 
-    /**
-     * Check domain's registry expiry date
-     */
-    CheckPaidTillDate(URL)
-        .then(date => {
-            core.setOutput("paid-till-date", date.toString());
-            core.setOutput("paid-till-days-left", Dates.countDays(date));
-        })
-        .catch(core.error);
+                throw error;
+            })
+            .catch(core.error);
+    }
+
+    if (CHECK_ACTION == "registry") {
+        core.setOutput("run-action", "registry");
+        /**
+         * Check domain's registry expiry date
+         */
+        CheckPaidTillDate(URL)
+            .then(date => {
+                core.setOutput("paid-till-date", date.toString());
+                core.setOutput("paid-till-days-left", Dates.countDays(date));
+            })
+            .catch(core.error);
+    }
 } catch (error) {
     core.setFailed(error.message);
 }
